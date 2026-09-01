52a76f296a9eac0fd9726cecaf70318a3bd6d9c6
GhOssTYyy <jpandreiph@gmail.com>
2026-09-01 10:07:17 -0300

    Adicionei a função para verificar qual periodo do ponto, porém ainda falta terminar o arquivo "clock_in_verification", para adicionar tudo isso para o usuário

--- clock_in_verification.js
+++ clock_in_verification.js
@@ -43,11 +43,39 @@ function convert_minutes_to_hours(minutes){
 function get_actual_hours(){
 
     const actual_hour = new Date()
+
+    return actual_hour
 }
 
-function discover_the_period_of_clock_in(){
+function get_actual_minutes(){
     
-    const actual_time = new Date()
-    const actual_time_conversion = clock_time.getHours * 60 + clock_time.getMinutes
+    const actual_minute = new Date()
+
+    return actual_minute.getHours() * 60 + actual_minute.getMinutes()
+}
+
+
+function discover_actual_clock_in_period(){
+
+    const actual_time_in_minutes = get_actual_minutes()
+
+    const entry = convert_time_to_minutes(times_of_clock_in.fixed_times.entry)
+    const begin_dinner = convert_time_to_minutes(times_of_clock_in.fixed_times.begin_dinner)
+    const ending_dinner = convert_time_to_minutes(times_of_clock_in.fixed_times.ending_dinner)
+    const exit = convert_time_to_minutes(times_of_clock_in.fixed_times.exit)
+
+
+
+    const start_entry_tolerance = entry - times_of_clock_in.tolerance.start_entry_tolerance
+    const entry_arrive_late_tolerance = entry + times_of_clock_in.tolerance.entry_arrive_late
+
+/*  const janela_almoco_inicio = begin_dinner - times_of_clock_in.tolerance.start_dinner_tolerance
+    const janela_almoco_fim = begin_dinner + times_of_clock_in.tolerance.dinner_arrive_late
+
+    const janela_volta_inicio = ending_dinner - times_of_clock_in.tolerance.start_return_tolerance
+    const janela_volta_fim = ending_dinner + times_of_clock_in.tolerance.return_arrive_late
+
+    const janela_saida_inicio = exit - times_of_clock_in.tolerance.start_exit_tolerance */
+    const extra_hours = exit + times_of_clock_in.tolerance.extra_hours
 }
 
